0b8fb685c91394d48951ddf75e68af2c9faffc11
Tim Hemmer <themmer@gmail.com>
2017-05-27 23:09:27 -0500

    Menu is working with shrink menu highlighting

--- src/assets/js/building-blocks/sticky-shrinknav.js
+++ src/assets/js/building-blocks/sticky-shrinknav.js
@@ -1,10 +1,27 @@
+// Sticky menu shrink logic
 $(function() {
+  const shrinkClass = 'sticky-shrinknav-wrapper';
+  const $body = $('body');
+  const headerSizeWithNoMenu = 150;
   $(window).scroll(function() {
-    var winTop = $(window).scrollTop();
-    if (winTop >= 30) {
-      $("body").addClass("sticky-shrinknav-wrapper");
-    } else{
-      $("body").removeClass("sticky-shrinknav-wrapper");
+    const hasShrunk = $body.hasClass(shrinkClass);
+    const winScrollTop = $(window).scrollTop();
+    const viewportHeight = $(window).outerHeight(true);
+    const documentHeight = $(document).outerHeight(true);
+    if (!hasShrunk && winScrollTop >= headerSizeWithNoMenu && documentHeight > viewportHeight + headerSizeWithNoMenu) {
+      $body.addClass(shrinkClass);
+    } else if (winScrollTop < 1 && hasShrunk) {
+      $body.removeClass(shrinkClass);
+    }
+  });
+});
+
+// Active menu highlighting
+$(function() {
+  const pageUrl = window.location.href.substr(window.location.href.lastIndexOf('/') + 1);
+  $('.menu li a').each(function() {
+    if ($(this).attr('href') == pageUrl || $(this).attr('href') == '') {
+      $(this).addClass('active');
     }
   });
 });
